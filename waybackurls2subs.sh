e844a409d8a0ba8c3adee3767e0531622e0c65c4
Hacktify Cyber Security <114749439+hacktifycs@users.noreply.github.com>
2023-06-30 14:45:45 +0530

    Create waybackurls2subs.sh

---
+++ waybackurls2subs.sh
@@ -0,0 +1,1 @@
+curl --insecure --silent "http://web.archive.org/cdx/search/cdx?url=*.$1/*&output=text&fl=original&collapse=urlkey" | sed -e 's_https*://__' -e "s/\/.*//" -e 's/:.*//' -e 's/^www\.//' | sed "/@/d" | sed -e 's/\.$//' | sort -u
